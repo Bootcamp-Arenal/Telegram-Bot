8c56d5eda0552a105d6c65b4176e61280b47e509
Jorge Luis Ruiz-Henestrosa Ruiz <rhr.jorge@gmail.com>
2022-11-13 02:59:12 +0100

    Bot doesn't stop once initialized

--- src/main/java/com/arenal/telegrambot/BootcampArenalBot.java
+++ src/main/java/com/arenal/telegrambot/BootcampArenalBot.java
@@ -1,8 +1,45 @@
 package com.arenal.telegrambot;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
-public class BootcampArenalBot {
+// @ConfigurationProperties(prefix = "telegram-bot")
+public class BootcampArenalBot extends TelegramLongPollingBot {
+
+    private String username;
+    private String token;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            return;
+        }
+        System.out.println(username + " " + token);
+        SendMessage sendMessage = new SendMessage();
+        if (update.getMessage().getText().equals("/start")) {
+            sendMessage.setText("Hello, " + update.getMessage().getFrom().getFirstName() + "!");
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+            try {
+                execute(sendMessage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5732632626:AAGbGOF26WCUxdidgNrixs5iGIiVFoQw_gE";
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "bootcamp_arenal_bot";
+    }
 
 }
--- src/main/java/com/arenal/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/arenal/telegrambot/TelegramBotApplication.java
@@ -2,6 +2,10 @@ package com.arenal.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegramBotApplication {
